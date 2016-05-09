f0d5f28a8dda9db034b6337c4c71aa315170ed59
Tarun Arora <tananrules@gmail.com>
2016-05-10 00:29:09 +0530

    bootstrap added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,6 +9,8 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
