4e72701fbdb008f76b7ff4843f84c1f88ad86c41
Tarun Arora <tananrules@gmail.com>
2016-05-10 01:12:49 +0530

    metisMenu added to navbar

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  setup: Ember.on('didInsertElement', function() {
+    // MetsiMenu
+    Ember.$('#side-menu').metisMenu();
+  })
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,20 +9,12 @@ module.exports = function(defaults) {
     }
   });
 
+  // Bootstrap
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  
+  // Metismenu
+  app.import('bower_components/metisMenu/dist/metisMenu.min.css');
+  app.import('bower_components/metisMenu/dist/metisMenu.min.js');
 
   return app.toTree();
 };
