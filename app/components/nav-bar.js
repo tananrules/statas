5659ce310af6b432c12b4e2b2198fa3627eeacc4
Tarun Arora <tananrules@gmail.com>
2016-05-11 01:40:42 +0530

    dashboard added

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,8 +1,40 @@
 import Ember from 'ember';
 
+const {
+  $
+} = Ember;
+
+function SmoothlyMenu() {
+  if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+    // Hide menu in order to smoothly turn on when maximize menu
+    $('#side-menu').hide();
+    // For smoothly turn on menu
+    setTimeout(
+      function () {
+        $('#side-menu').fadeIn(400);
+      }, 
+    200);
+  } else if ($('body').hasClass('fixed-sidebar')) {
+      $('#side-menu').hide();
+      setTimeout(
+        function () {
+          $('#side-menu').fadeIn(400);
+        }, 
+      100);
+  } else {
+    // Remove all inline style from jquery fadeIn function to reset menu state
+    $('#side-menu').removeAttr('style');
+  }
+}
+
 export default Ember.Component.extend({
   setup: Ember.on('didInsertElement', function() {
     // MetsiMenu
-    Ember.$('#side-menu').metisMenu();
+    $('#side-menu').metisMenu();
+
+    $('.navbar-minimalize').click(function () {
+        $("body").toggleClass("mini-navbar");
+        SmoothlyMenu();
+    });
   })
 });
