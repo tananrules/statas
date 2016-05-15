4c85e068320b4801815a72d21f61d290826a829d
Tarun Arora <tananrules@gmail.com>
2016-05-15 23:30:49 +0530

    fake data used for users

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,11 +8,6 @@ const {
 export default Ember.Controller.extend({
   activeFilter: 'all',
 
-  // all: computed('activeFilter', function() {
-  //   debugger
-  //   return this.get('activeFilter') === 'all';
-  // }),
-
   actions: {
     filterUsers(type) {
       this.set('activeFilter', type);
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      users: [
+      {
+        name: 'John Smith',
+        type: 'am',
+        active_rev: '$100,000',
+        lte: '$320,000',
+        rp: 19
+      },
+      {
+        name: 'Lucas Bean',
+        type: 'im',
+        active_rev: '$50,000',
+        lte: '$120,000',
+        rp: 10
+      }]
+    }
+  }
 });
