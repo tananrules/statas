c1516dc5563b10de8ed37ff2283e2eac822d6be7
Tarun Arora <tananrules@gmail.com>
2016-05-17 16:40:27 +0530

    dashboard table fixed for dummy data

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      offers: [
+        {
+          name: "Atari",
+          description: "I Love Strawberries",
+          icon: "mobile-phone",
+          amount: "12,000,000",
+          time_remaining: "05 12 45"
+        },
+        {
+          name: "Apple",
+          description: "itunes fest",
+          icon: 'music',
+          amount: "10,000,000",
+          time_remaining: "02 05 32"
+        }
+      ]
+    };
+  }
 });
--- app/routes/users.js
+++ app/routes/users.js
@@ -4,20 +4,21 @@ export default Ember.Route.extend({
   model() {
     return {
       users: [
-      {
-        name: 'John Smith',
-        type: 'am',
-        active_rev: '$100,000',
-        lte: '$320,000',
-        rp: 19
-      },
-      {
-        name: 'Lucas Bean',
-        type: 'im',
-        active_rev: '$50,000',
-        lte: '$120,000',
-        rp: 10
-      }]
-    }
+        {
+          name: 'John Smith',
+          type: 'am',
+          active_rev: '$100,000',
+          lte: '$320,000',
+          rp: 19
+        },
+        {
+          name: 'Lucas Bean',
+          type: 'im',
+          active_rev: '$50,000',
+          lte: '$120,000',
+          rp: 10
+        }
+      ]
+    };
   }
 });
