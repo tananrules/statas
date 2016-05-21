4fda6c013206280a540f3ef1250aaa835c5528c2
Tarun Arora <tananrules@gmail.com>
2016-05-22 03:24:12 +0530

    logout outlet fixed

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  renderTemplate: function() {
+    this.render('login', {
+      into: 'application',
+      outlet: 'login'
+    })
+  }
 });
