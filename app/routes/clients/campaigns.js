36be727bbf48d3ab881d60d14dca1646c2119b0d
Tarun Arora <tananrules@gmail.com>
2016-06-01 03:51:43 +0530

    offers and campaign WIP

--- app/routes/clients/campaigns.js
+++ app/routes/clients/campaigns.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate() {
+    this.render('clients.campaigns', { into: 'application' });
+  },
   model() {
     return this.store.findAll('offer');
   }
--- app/routes/clients/offers.js
+++ app/routes/clients/offers.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  renderTemplate() {
+    this.render('clients.offers', { into: 'application' });
+  },
 });
