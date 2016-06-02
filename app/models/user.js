3944c740b35b6802677aa9345a35050cda99497e
Tarun Arora <tananrules@gmail.com>
2016-06-02 19:26:20 +0530

    attributed in model added

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,16 @@
 import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
 
 export default Model.extend({
-  
+  name: attr('string'),
+  type: attr('string'),
+  address: attr('string'),
+  suite: attr('string'),
+  city: attr('string'),
+  state: attr('string'),
+  zipCode: attr('string'),
+  contactFirstName: attr('string'),
+  contactLastName: attr('string'),
+  contactEmail: attr('string'),
+  contachPhone: attr('string')
 });
