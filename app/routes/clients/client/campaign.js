1546a9911693b90716b728117ff62c9e9bcd2ec3
Tarun Arora <tananrules@gmail.com>
2016-06-24 19:24:09 +0530

    campaigns page i1

--- app/routes/clients/client/campaign.js
+++ app/routes/clients/client/campaign.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   renderTemplate() {
     this.render('clients.client.campaign', {into: 'application'});
-  }
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    let parentModel = this.modelFor('clients.client');
+    controller.set('parentModel', parentModel);
+  },
+  model(params) {
+    return this.store.findRecord('campaign', params.campaign_id);
+  },
 });
