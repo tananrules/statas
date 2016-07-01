f5016d96b92904fb656519d05d742606bd770615
Tarun Arora <tananrules@gmail.com>
2016-07-01 17:31:55 +0530

    serializer response fixed

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -4,5 +4,12 @@ import RESTSerializer from 'ember-data/serializers/rest';
 export default RESTSerializer.extend({
   serializeIntoHash: function(data, type, record, options) {
     Ember.merge(data, this.serialize(record, options));
+  },
+  normalizeFindRecordResponse (store, primaryModelClass, payload, id, requestType) {
+    let modelName = primaryModelClass.modelName;
+    let newPayload = {};
+    newPayload[modelName] = payload;
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
   }
 });
+  
