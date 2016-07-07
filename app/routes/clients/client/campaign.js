5db8cd8f962cbd880b196b768177262e9afa55ef
Tarun Arora <tananrules@gmail.com>
2016-07-07 23:27:23 +0530

    no model message display

--- app/routes/clients/client/campaign.js
+++ app/routes/clients/client/campaign.js
@@ -4,13 +4,13 @@ export default Ember.Route.extend({
   renderTemplate() {
     this.render('clients.client.campaign', {into: 'application'});
   },
-  // setupController(controller, model) {
-  //   this._super(controller, model);
-  //   // let parentModel = this.modelFor('clients.client');
-  //   // controller.set('parentModel', parentModel);
-  //   let offers = this.store.peekAll('offer');
-  //   controller.set('offers', offers.filterBy('campaignID', model.get('id'))); 
-  // },
+  setupController(controller, model) {
+    this._super(controller, model);
+    //Another Network Request (can be fixed for optimization)
+    let offers = this.store.findAll('offer').then((offers) => {
+      controller.set('offers', offers.filterBy('campaignID', model.get('id'))); 
+    });
+  },
   model(params) {
     return this.store.findRecord('campaign', params.campaign_id);
   },
