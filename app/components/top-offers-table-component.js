51505d9803a2643e5c1fb69293d6b8e5d1318a17
Tarun Arora <tananrules@gmail.com>
2016-07-11 03:09:52 +0530

    offer approval added

--- app/components/top-offers-table-component.js
+++ app/components/top-offers-table-component.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  showActions: false
+  showActions: false,
+  selectedOffer: {},
+
+  actions: {
+    openApproveOfferModal(offer) {
+      this.set('selectedOffer.id', offer.get('id'));
+      this.set('selectedOffer.name', offer.get('name'));
+      Ember.$('#approveOfferModal').modal('show');
+    },
+
+    approveOffer() {
+      let offerId = this.get('selectedOffer.id');
+      let offer = this.store.peekRecord('offer', offerId);
+      offer.set('approved', true);
+      offer.save().then(() => {
+        Ember.$('#approveOfferModal').modal('hide');
+      });
+    }
+  }
 });
--- app/models/offer.js
+++ app/models/offer.js
@@ -19,9 +19,28 @@ export default Model.extend({
   reachNeeded: attr(),
   status: attr(),
   active: attr('boolean'),
+  approved: attr('boolean'),
+  createdAt: attr('date', {
+    defaultValue() { return new Date(); }
+  }),
 
-  // time_remaining: attr(),
-  
-  // campaign: belongsTo('campaign'),
-  // advertiser: belongsTo('advertiser'),
+  timeRemaining: Ember.computed('createdAt', function() {    
+    let  delta = Math.abs(Date.now() - Date.parse(this.get('createdAt'))) / 1000;
+    let days = Math.floor(delta / 86400);
+    if (days.toString().length === 1) {
+      days = `0${days}`;
+    }
+    delta -= days * 86400;
+    let hours = Math.floor(delta / 3600) % 24;
+    if (hours.toString().length === 1) {
+      hours = `0${hours}`;
+    }
+    delta -= hours * 3600;
+    let minutes = Math.floor(delta / 60) % 60;
+    if (minutes.toString().length === 1) {
+      minutes = `0${minutes}`;
+    }
+    delta -= minutes * 60;
+    return `${days} ${hours} ${minutes}`; 
+  })
 });
